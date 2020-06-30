39ba1465d17f5f74a93ee292961ea4b8cb161b2b
Grant Orchard <go@hashicorp.com>
2020-07-01 09:39:46 +1000

    initial commit, Azure dynamic credentials and resource group creation

--- main.tf
+++ main.tf
@@ -1,1 +1,37 @@
+#provider tfe {}
+#provider github {}
+provider vault {}
+provider azurerm {
+  features {}
+}
 
+/***************************************************
+1. Functional specs.
+
+Thinking through the flow here, what user inputs are
+we going to get, and what are we going to do with
+those?
+
+var.sensitivity will be one of (a, b, c) and will dictate the Vault path to be used when creating Accounts
+
+*/
+
+resource azurerm_resource_group "this" {
+  name     = "foo"
+  location = "australiaeast"
+}
+
+resource vault_azure_secret_backend_role "this" {
+  backend                     = "azure-prod"
+  role                        = "generated_role"
+
+  azure_roles {
+    role_name = "Contributor"
+    scope =  azurerm_resource_group.this.id
+  }
+}
+
+
+data vault_generic_secret "this" {
+  path = vault_azure_secret_backend_role.this.id
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,14 @@
+variable subscription_id {
+  type    = map(string)
+  default = {
+    production  = "14692f20-9428-451b-8298-102ed4e39c2a"
+    development = ""
+    staging     = ""
+    test        = ""
+  }
+}
 
+variable environment {
+  type    = string
+  default = "production"
+}
